c7b62c8cc8d7dd38379e38e4de467d99b8de6c16
fell-z <matheusfelipeagapito@hotmail.com>
2023-04-02 15:43:16 -0300

    Add a base file to all chess pieces inherit from

---
+++ lib/chess_pieces/piece_base.rb
@@ -0,0 +1,60 @@
+# This base class has all methods and constants that are common to all standard chess pieces.
+class PieceBase
+  RANK = 0
+  FILE = 1
+
+  DIRECTIONS = {
+    up: [-1, 0],
+    down: [1, 0],
+    right: [0, 1],
+    left: [0, -1],
+    up_left: [-1, -1],
+    up_right: [-1, 1],
+    down_left: [1, -1],
+    down_right: [1, 1]
+  }.freeze
+
+  attr_reader :side, :position
+
+  def initialize(board, side, position)
+    @board = board
+    @side = side
+    @position = position
+
+    @board.place_piece_at(@position, self)
+  end
+
+  def move(dest_pos)
+    return :failure unless possible_moves.include?(dest_pos)
+
+    @board.move_piece(@position, dest_pos)
+    @position = dest_pos
+
+    :success
+  end
+
+  def capture(piece_pos)
+    return :failure unless possible_captures.include?(piece_pos)
+
+    @board.remove_piece_at(piece_pos)
+
+    @board.move_piece(@position, piece_pos)
+    @position = piece_pos
+
+    :success
+  end
+
+  private
+
+  def squares_towards_to(direction)
+    squares = []
+    current_position = @position.dup
+
+    while current_position.all? { |value| value.between?(0, 7) }
+      current_position.map!.with_index { |value, index| value + direction[index] }
+      squares << current_position.dup if current_position.all? { |value| value.between?(0, 7) }
+    end
+
+    squares
+  end
+end
