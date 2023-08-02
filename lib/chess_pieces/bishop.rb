b6babd9419c051e29c37f5ff7f55f6b2dd684d7b
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-02 20:29:42 -0300

    Implement the #possible_moves method for the bishop
    - Also added the #to_s method with the representation of the bishop

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -1,4 +1,28 @@
 require_relative "./piece_base"
 
+# Implementation of chess bishop
 class Bishop < PieceBase
+  def possible_moves
+    moves = []
+
+    directions.each_value do |direction|
+      squares_towards_to(direction).each do |square|
+        break unless @board.at_position(square).nil?
+
+        moves << square
+      end
+    end
+
+    moves
+  end
+
+  def to_s
+    "♝"
+  end
+
+  private
+
+  def directions
+    DIRECTIONS.select { |direction| %i[up_left down_left up_right down_right].include?(direction) }
+  end
 end
