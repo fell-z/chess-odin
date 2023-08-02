2403aefcb18690877e0853590941aa026edce4a4
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-02 20:27:43 -0300

    Add specs regarding the bishop's #possible_moves

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -10,4 +10,44 @@ describe Bishop do
   subject(:bishop) { described_class.new(board, :white, position) }
 
   it_behaves_like "a subclass of PieceBase"
+
+  describe "#possible_moves" do
+    context "when it's in the" do
+      context "one of the default positions, c8" do
+        let(:position) { [0, 2] }
+
+        it "doesn't include those invalid moves near itself" do
+          computed_moves = bishop.possible_moves
+          expect(computed_moves).to_not include([2, 1], [2, 3], [0, 1], [0, 3], [1, 2], [2, 2])
+        end
+      end
+
+      context "one of the default positions, f8" do
+        let(:position) { [0, 5] }
+
+        it "doesn't include those invalid moves near itself" do
+          computed_moves = bishop.possible_moves
+          expect(computed_moves).to_not include([0, 4], [0, 6], [1, 5], [2, 5], [2, 4], [2, 6])
+        end
+      end
+
+      context "one of the default positions, c1" do
+        let(:position) { [7, 2] }
+
+        it "doesn't include those invalid moves near itself" do
+          computed_moves = bishop.possible_moves
+          expect(computed_moves).to_not include([7, 1], [7, 3], [6, 2], [5, 2], [5, 1], [5, 3])
+        end
+      end
+
+      context "one of the default positions, f1" do
+        let(:position) { [7, 5] }
+
+        it "doesn't include those invalid moves near itself" do
+          computed_moves = bishop.possible_moves
+          expect(computed_moves).to_not include([7, 4], [7, 6], [6, 5], [5, 5], [5, 4], [5, 6])
+        end
+      end
+    end
+  end
 end
