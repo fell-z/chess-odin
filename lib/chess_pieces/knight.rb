6fc42d9419d9bc597b92e8bbe849ac90cff507fa
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-05 18:23:55 -0300

    Implement #possible_moves for the knight class

--- lib/chess_pieces/knight.rb
+++ lib/chess_pieces/knight.rb
@@ -2,7 +2,36 @@ require_relative "./piece_base"
 
 # Implementation of chess knight
 class Knight < PieceBase
+  def possible_moves
+    moves = []
+
+    directions.each_value do |direction|
+      new_position = [@position, direction].transpose.map(&:sum)
+
+      next unless new_position.all? { |value| value.between?(0, 7) }
+
+      moves << new_position if @board.at_position(new_position).nil?
+    end
+
+    moves
+  end
+
   def to_s
     "♞"
+  end
+
+  private
+
+  def directions
+    {
+      up_a: [-2, -1],
+      up_b: [-2, 1],
+      down_a: [2, -1],
+      down_b: [2, 1],
+      left_a: [-1, -2],
+      left_b: [1, -2],
+      right_a: [-1, 2],
+      right_b: [1, 2]
+    }
   end
 end
