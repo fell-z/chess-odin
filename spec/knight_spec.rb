180afecdb8efcfc624612fc7e4f01db254186457
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-05 18:11:48 -0300

    Add some initial knight's #possible_moves specs

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -10,4 +10,24 @@ describe Knight do
   subject(:knight) { described_class.new(board, :white, position) }
 
   it_behaves_like "a subclass of PieceBase"
+
+  describe "#possible_moves" do
+    context "when it's in" do
+      context "one of the default positions, in b8" do
+        let(:position) { [0, 1] }
+
+        it "includes the positions c6 and d7" do
+          computed_moves = knight.possible_moves
+          valid_moves = [[2, 2], [1, 3]]
+          expect(computed_moves).to include(*valid_moves)
+        end
+
+        it "doesn't include the positions c7 and e7" do
+          computed_moves = knight.possible_moves
+          invalid_moves = [[1, 2], [1, 4]]
+          expect(computed_moves).to_not include(*invalid_moves)
+        end
+      end
+    end
+  end
 end
