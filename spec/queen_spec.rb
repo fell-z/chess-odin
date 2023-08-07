ebe38fed7c3bd717e068c80be16758fa504707cb
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-06 22:31:17 -0300

    Update the queen's #possible_moves specs
    - Added some common cases to test

--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -27,6 +27,61 @@ describe Queen do
           valid_moves = [[3, 0], [0, 5], [4, 3]]
           expect(computed_moves).to include(*valid_moves)
         end
+
+        context "with two pieces obstructing the way, in f6 and b8" do
+          before do
+            described_class.new(board, :white, [2, 5])
+            described_class.new(board, :white, [0, 1])
+          end
+
+          it "doesn't include the positions f6, b8 and the ones after it" do
+            computed_moves = queen.possible_moves
+            invalid_moves = [[2, 5], [3, 6], [0, 1], [0, 0]]
+            expect(computed_moves).to_not include(*invalid_moves)
+          end
+
+          it "includes the positions c8 and e7" do
+            computed_moves = queen.possible_moves
+            valid_moves = [[0, 2], [1, 4]]
+            expect(computed_moves).to include(*valid_moves)
+          end
+        end
+      end
+
+      context "the center, in d4" do
+        let(:position) { [4, 3] }
+
+        it "doesn't include the positions b5 and e2" do
+          computed_moves = queen.possible_moves
+          invalid_moves = [[3, 1], [6, 4]]
+          expect(computed_moves).to_not include(*invalid_moves)
+        end
+
+        it "includes the positions b4, d5, f6 and a1" do
+          computed_moves = queen.possible_moves
+          valid_moves = [[4, 1], [3, 3], [2, 5], [7, 0]]
+          expect(computed_moves).to include(*valid_moves)
+        end
+
+        context "with three pieces obstructing the way, in d3, b6 and a4" do
+          before do
+            described_class.new(board, :white, [5, 3])
+            described_class.new(board, :white, [2, 1])
+            described_class.new(board, :white, [4, 0])
+          end
+
+          it "doesn't include the positions d3, b6, a4 and the ones after it" do
+            computed_moves = queen.possible_moves
+            invalid_moves = [[5, 3], [2, 1], [1, 0], [4, 0]]
+            expect(computed_moves).to_not include(*invalid_moves)
+          end
+
+          it "includes the positions c5 and b4" do
+            computed_moves = queen.possible_moves
+            valid_moves = [[3, 2], [4, 1]]
+            expect(computed_moves).to include(*valid_moves)
+          end
+        end
       end
     end
   end
