fcc75b82e0ddb5195afcfe64f624acffd1d8c342
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-06 21:25:42 -0300

    Implement #possible_moves for the queen class

--- lib/chess_pieces/queen.rb
+++ lib/chess_pieces/queen.rb
@@ -2,6 +2,20 @@ require_relative "./piece_base"
 
 # Implementation of chess queen
 class Queen < PieceBase
+  def possible_moves
+    moves = []
+
+    DIRECTIONS.each_value do |direction|
+      squares_towards_to(direction).each do |square|
+        break unless @board.at_position(square).nil?
+
+        moves << square
+      end
+    end
+
+    moves
+  end
+
   def to_s
     "♛"
   end
