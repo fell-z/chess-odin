902e983171a23873469402c00e07e810e798b596
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-07 21:02:33 -0300

    Implement #possible_captures for the queen class

--- lib/chess_pieces/queen.rb
+++ lib/chess_pieces/queen.rb
@@ -16,6 +16,21 @@ class Queen < PieceBase
     moves
   end
 
+  def possible_captures
+    captures = []
+
+    DIRECTIONS.each_value do |direction|
+      squares_towards_to(direction).each do |square|
+        next if @board.at_position(square).nil?
+
+        captures << square if @board.at_position(square).side != @side
+        break
+      end
+    end
+
+    captures
+  end
+
   def to_s
     "♛"
   end
