844e3a232f1288aec3dd3d296eab2beab4dfef23
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-13 21:28:54 -0300

    Add some initial king's #possible_moves specs

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -10,4 +10,24 @@ describe King do
   subject(:king) { described_class.new(board, :white, position) }
 
   it_behaves_like "a subclass of PieceBase"
+
+  describe "#possible_moves" do
+    context "when it's in" do
+      context "one of the default positions, e8" do
+        let(:position) { [0, 4] }
+
+        it "doesn't include the positions c6, g8 and e5" do
+          computed_moves = king.possible_moves
+          invalid_moves = [[2, 2], [0, 6], [3, 4]]
+          expect(computed_moves).to_not include(*invalid_moves)
+        end
+
+        it "includes the positions d7, e7 and f8" do
+          computed_moves = king.possible_moves
+          valid_moves = [[1, 3], [1, 4], [0, 5]]
+          expect(computed_moves).to include(*valid_moves)
+        end
+      end
+    end
+  end
 end
