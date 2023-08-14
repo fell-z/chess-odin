f25002ab48589096123d2531602cd9d38145a9ed
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-13 21:32:07 -0300

    Implement #possible_moves for the king class

--- lib/chess_pieces/king.rb
+++ lib/chess_pieces/king.rb
@@ -2,6 +2,20 @@ require_relative "./piece_base"
 
 # Implementation of chess king
 class King < PieceBase
+  def possible_moves
+    moves = []
+
+    DIRECTIONS.each_value do |direction|
+      new_position = [@position, direction].transpose.map(&:sum)
+
+      next unless new_position.all? { |value| value.between?(0, 7) }
+
+      moves << new_position if @board.at_position(new_position).nil?
+    end
+
+    moves
+  end
+
   def to_s
     "♚"
   end
