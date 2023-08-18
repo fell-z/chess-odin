7f076b2ce52b994d503adaf07d2f9571148084ae
fell-z <matheusfelipeagapito@hotmail.com>
2023-08-17 22:02:54 -0300

    Refactor similar functioning classes pt.2
    - The #possible_moves and #possible_captures of King and Knight
    classes were moved to a module, due to their similarity in functioning

--- lib/chess_pieces/king.rb
+++ lib/chess_pieces/king.rb
@@ -1,40 +1,18 @@
 require_relative "./modules/piece_base"
+require_relative "./modules/special_movement"
 
 # Implementation of chess king
 class King
   include PieceBase
-
-  def possible_moves
-    moves = []
-
-    DIRECTIONS.each_value do |direction|
-      new_position = [@position, direction].transpose.map(&:sum)
-
-      next unless new_position.all? { |value| value.between?(0, 7) }
-
-      moves << new_position if @board.at_position(new_position).nil?
-    end
-
-    moves
-  end
-
-  def possible_captures
-    captures = []
-
-    DIRECTIONS.each_value do |direction|
-      new_capture = [@position, direction].transpose.map(&:sum)
-
-      next unless new_capture.all? { |value| value.between?(0, 7) }
-
-      unless @board.at_position(new_capture).nil? || @board.at_position(new_capture).side == @side
-        next captures << new_capture
-      end
-    end
-
-    captures
-  end
+  include SpecialMovement
 
   def to_s
     "♚"
+  end
+
+  private
+
+  def directions
+    DIRECTIONS
   end
 end
---
+++ lib/chess_pieces/modules/special_movement.rb
@@ -0,0 +1,38 @@
+# This module contains the #possible_moves and #possible_captures specific to the King and Knight classes
+module SpecialMovement
+  def possible_moves
+    moves = []
+
+    directions.each_value do |direction|
+      new_position = add(@position, direction)
+
+      next unless new_position.all? { |value| value.between?(0, 7) }
+
+      moves << new_position if @board.at_position(new_position).nil?
+    end
+
+    moves
+  end
+
+  def possible_captures
+    captures = []
+
+    directions.each_value do |direction|
+      new_capture = add(@position, direction)
+
+      next unless new_capture.all? { |value| value.between?(0, 7) }
+
+      unless @board.at_position(new_capture).nil? || @board.at_position(new_capture).side == @side
+        next captures << new_capture
+      end
+    end
+
+    captures
+  end
+
+  private
+
+  def add(position, direction)
+    [position, direction].transpose.map(&:sum)
+  end
+end
