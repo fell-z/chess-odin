24e164f65b951433c0cddd0a9e77200e4f5ecdb3
fell-z <matheusfelipeagapito@hotmail.com>
2023-10-25 12:59:01 -0300

    Add specs for the promoting system of pawn class

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -1,5 +1,5 @@
 require_relative "./piece_base_shared_spec"
-require_relative "../lib/chess_pieces/pawn"
+require_relative "../lib/chess_pieces"
 require_relative "../lib/board"
 
 # rubocop:disable Metrics
@@ -147,6 +147,97 @@ describe Pawn do
           result = pawn.possible_captures
           expect(result).to be_empty
         end
+      end
+    end
+  end
+
+  describe "#promote?" do
+    context "with the pawn in e2" do
+      before { pawn.instance_variable_set(:@move_direction, :up) }
+      let(:position) { [1, 4] }
+
+      it "returns true after moving to e1" do
+        expect { pawn.move([0, 4]) }.to change { pawn.promote? }.to(true)
+      end
+    end
+
+    context "with the pawn in e7" do
+      before { pawn.instance_variable_set(:@move_direction, :down) }
+      let(:position) { [6, 4] }
+
+      it "returns true after moving to e8" do
+        expect { pawn.move([7, 4]) }.to change { pawn.promote? }.to(true)
+      end
+    end
+  end
+
+  describe "#promote" do
+    subject(:promoting_piece) { described_class.new(board, :white, position) }
+
+    before do
+      @original_position = promoting_piece.position
+      @original_side = promoting_piece.side
+    end
+
+    context "when promoting to a rook" do
+      it "is really a rook" do
+        promoted_piece = promoting_piece.promote(Rook)
+        expect(promoted_piece).to be_a(Rook)
+      end
+
+      it "reappears in the same place, with the same attributes" do
+        promoted_piece = promoting_piece.promote(Rook)
+        expect(promoted_piece).to have_attributes(position: @original_position, side: @original_side)
+      end
+    end
+
+    context "when promoting to a bishop" do
+      it "is really a bishop" do
+        promoted_piece = promoting_piece.promote(Bishop)
+        expect(promoted_piece).to be_a(Bishop)
+      end
+
+      it "reappears in the same place, with the same attributes" do
+        promoted_piece = promoting_piece.promote(Bishop)
+        expect(promoted_piece).to have_attributes(position: @original_position, side: @original_side)
+      end
+    end
+
+    context "when promoting to a knight" do
+      it "is really a knight" do
+        promoted_piece = promoting_piece.promote(Knight)
+        expect(promoted_piece).to be_a(Knight)
+      end
+
+      it "reappears in the same place, with the same attributes" do
+        promoted_piece = promoting_piece.promote(Knight)
+        expect(promoted_piece).to have_attributes(position: @original_position, side: @original_side)
+      end
+    end
+
+    context "when promoting to a queen" do
+      it "is really a queen" do
+        promoted_piece = promoting_piece.promote(Queen)
+        expect(promoted_piece).to be_a(Queen)
+      end
+
+      it "reappears in the same place, with the same attributes" do
+        promoted_piece = promoting_piece.promote(Queen)
+        expect(promoted_piece).to have_attributes(position: @original_position, side: @original_side)
+      end
+    end
+
+    context "when trying promoting to a pawn" do
+      it "is the same pawn object" do
+        promoted_piece = promoting_piece.promote(Pawn)
+        expect(promoted_piece).to be(promoting_piece)
+      end
+    end
+
+    context "when trying promoting to a king" do
+      it "is the same pawn object" do
+        promoted_piece = promoting_piece.promote(King)
+        expect(promoted_piece).to be(promoting_piece)
       end
     end
   end
